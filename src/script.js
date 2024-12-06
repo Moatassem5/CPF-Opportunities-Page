69094729717bde308c8434b841c909d70735c672
Moatassem Ahmed <moatassemahmed582@gmail.com>
2024-12-06 21:47:43 +0200

    Implementing Responsive design

--- src/script.js
+++ src/script.js
@@ -1,22 +1,33 @@
-'use strict'
+"use strict";
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+const nav = document.querySelector("nav");
+const buttonJoin = document.querySelector(".btn-join");
 
-function scaleOut(event) {
-    const card = event.currentTarget;
-    card.classList.add('hide'); // Trigger scale-down animation
-    card.addEventListener('transitionend', () => {
-        card.style.display = 'none'; // Hide after the animation
-    }, { once: true });
-}
+const movingJoinButton = function () {
+  if (window.innerWidth <= 900) {
+    if (!navList.contains(buttonJoin)) {
+      navList.appendChild(buttonJoin);
+    }
+  } else {
+    if (!nav.contains(buttonJoin)) {
+      nav.insertBefore(buttonJoin, hamburger);
+    }
+  }
+};
 
-function resetCard(event) {
-    const card = event.currentTarget;
-    card.classList.remove('hide');
-    card.style.display = ''; // Reset display property
-}
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.addEventListener('touchstart', scaleOut);
-    card.addEventListener('touchend', resetCard);
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
 });
+
+// Close menu when clicking a link
+document.querySelectorAll(".list-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+  })
+);
+
+window.addEventListener('load',movingJoinButton)
+window.addEventListener('resize',movingJoinButton)
